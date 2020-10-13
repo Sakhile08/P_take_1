b742c1b77a1c1966d48f101019eb6ed42b61ee91
Sakhile08 <68563996+Sakhile08@users.noreply.github.com>
2020-10-14 01:20:36 +0200

    Add files via upload

---
+++ 11. Task 11.py
@@ -0,0 +1,6 @@
+s1 = ("care")
+s2 = ("bear")
+a=list(set(s1)& set(s2))
+print ('the common letters are: ')
+for i in a:
+ print (i)
---
+++ 8. Task 8.py
@@ -0,0 +1,7 @@
+num = int(input("Enter the number = "))
+hour = num//60
+hour1 = num%60
+minute = hour1%60
+
+print(hour, "hours", minute, "minutes")
+
